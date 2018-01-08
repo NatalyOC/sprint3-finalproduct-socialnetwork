d6a4184ef7e56f22fd9c45553d805527959f4f58
NatalyOC <noteroc@hotmail.com>
2018-01-08 18:17:32 -0500

    agrregando funcionalidad

--- js/publications.js
+++ js/publications.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
+  var refComments = '';
+  
+  var contenido=$('#public-users');
+  refComments = firebase.database().ref().child('recentComments');
+ 
+ $('#bt').on('click',function(){
+    showData();
+ });
+  function showData() {
+    refComments.on('value', function(snap) {
+      var data = snap.val();
+      var filaAMostrar = '';
+      for (var key in data) {
+        filaAMostrar += '<div>'+data[key].comment+'<div>'; 
+      }
+      contenido.innerHTML=filaAMostrar;
+    });
+  }
 });
