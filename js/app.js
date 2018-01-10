fb0cdede48653d670fe86ae652cead487752f4c5
NatalyOC <noteroc@hotmail.com>
2018-01-10 17:10:07 -0500

    añadiendo funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+  // Controlador de evento del elemento button login
   $('#login').on('click', function() {
     $(location).attr('href', 'views/login.html');
+  });
+  // Controlador de evento del elemento button signup
+  $('#button-signup').on('click',function() {
+    alert('Registro Sastifactorio');
+    $(location).attr('href', 'views/login.html');
+  });
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
   });
 });
