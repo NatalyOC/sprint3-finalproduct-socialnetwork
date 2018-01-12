52ce29860b5a20af76c37e0b107f586b3518c343
darcycervh <darcycervera@gmail.com>
2018-01-12 14:26:53 -0500

    agregando view signup

---
+++ js/signup.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#btn-signup').on('click', function() {
+
+  });
+  $('#logo-heart').on('click',function() {
+    $('#logo-heart').attr('href', '../index.html');
+  })
+});
