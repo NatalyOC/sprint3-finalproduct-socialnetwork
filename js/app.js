d75fbed3dec5fe018df2e1f3f2d8f506149cafec
darcycervh <darcycervera@gmail.com>
2018-01-14 01:49:51 -0500

    validando formulario

--- js/app.js
+++ js/app.js
@@ -3,12 +3,64 @@ $(document).ready(function() {
   $('#login').on('click', function() {
     $(location).attr('href', 'views/login.html');
   });
-  // Controlador de evento del elemento button signup
-  $('#button-signup').on('click',function() {
-    alert('Registro Sastifactorio');
-    $(location).attr('href', 'views/login.html');
-  });
-  $('#signup').on('click', function() {
+  // Controlador de evento del elemento button login en el header
+  var $buttonSignup = $('#signup');
+  $buttonSignup.on('click', function() {
     $(location).attr('href', 'views/signup.html');
-  });  
+  }); 
+  // validar formulario del index
+  var $nameInput = $('#name');
+  var $emailInput = $('#email');
+  var $passwordInput = $('#password');
+  var $spanForm = $('#span-signup');
+  // -----------------------
+  var verifyName = false;
+  var verifyEmail = false;
+  var verifyPassword = false;
+  // -----------------------
+  $nameInput.on('input', function(event) {
+    verifyName = true;
+    activeButton();
+  }); 
+  $emailInput.on('input', function(event) {
+    verifyEmail = true;
+    activeButton();
+  });
+  $passwordInput.on('input', function(event) {
+    // console.log($passwordInput.val());
+    var $passwordValue = $passwordInput.val();
+    var regExpPassword = new RegExp(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/);
+    // console.log(regExpPassword.test($passwordValue));
+    /**
+     * Contraseñas que contengan al menos una letra mayúscula.
+Contraseñas que contengan al menos una letra minúscula.
+Contraseñas que contengan al menos un número o caracter especial.
+Contraseñas cuya longitud sea como mínimo 8 caracteres.
+Contraseñas cuya longitud máxima no debe ser arbitrariamente limitada.
+aA789ggg     
+*/
+    if (regExpPassword.test($passwordValue)) {
+      verifyPassword = true;
+      activeButton();
+      $('#span-signup').fadeOut();
+    } else {
+      $('#span-signup').fadeIn();
+      desactiveButton();
+    }
+  });
+  function activeButton(event) {
+    if (verifyName && verifyEmail && verifyPassword) {
+      $('#button-signup').css('background', 'teal');     
+    }
+  }
+  function desactiveButton() {
+    if (verifyName && verifyEmail && verifyPassword) {
+      $('#button-signup').attr('disabled', true);
+    }
+  }
+  $('#button-signup').on('click', function(event) {
+    event.preventDefault();
+    console.log(event);
+    $(location).attr('href', '../views/newsfeed.html');
+  });
 });
--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,50 @@
 $(document).ready(function() {
-  $('#btn-signup').on('click', function() {
-
+  // declarando variables para los inputs del formulario
+  var $nameInput2 = $('#name-2');
+  var $emailInput2 = $('#email-2');
+  var $passwordInput2 = $('#password-2');
+  // ----------------------------------------------
+  var verifyName2 = false;
+  var verifyEmail2 = false;
+  var verifyPassword2 = false;
+  // ----------------------------------------------
+  $nameInput2.on('input', function() {
+    verifyName2 = true;
+    activeButton2();
   });
-  $('#logo-heart').on('click',function() {
+  $emailInput2.on('input', function() {
+    verifyEmail2 = true;
+    activeButton2();
+  });
+  $passwordInput2.on('input', function(event) {
+    // console.log($passwordInput.val());
+    var $passwordValue2 = $passwordInput2.val();
+    var regExpPassword2 = new RegExp(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/);
+    console.log(regExpPassword2.test($passwordValue2));
+    if (regExpPassword2.test($passwordValue2)) {
+      verifyPassword2 = true;
+      activeButton2();
+    } else {
+      desactiveButton2();
+    }
+  });
+  function activeButton2(event) {
+    if (verifyName2 && verifyEmail2 && verifyPassword2) {
+      $('#signup-button').attr('disabled', false);   
+    }
+  }
+  function desactiveButton2() {
+    if (verifyName2 && verifyEmail2 && verifyPassword2) {
+      $('#signup-button').attr('disabled', true);
+    }
+  }
+  // ----------------------------------------------
+  $('#logo-heart').on('click', function() {
     $('#logo-heart').attr('href', '../index.html');
-  })
+  });
+  $('#signup-button').on('click', function(event) {
+    event.preventDefault();
+    console.log(event);
+    $(location).attr('href', 'newsfeed.html');
+  });
 });
